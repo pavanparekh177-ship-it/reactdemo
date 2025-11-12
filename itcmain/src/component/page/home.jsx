5bddd5efdb1b34f0d3781442e8acd81b7f74046f
Pavan Parekh <pavanparekh177@gmail.com>
2025-11-12 18:44:58 +0530

    home

--- itcmain/src/component/page/home.jsx
+++ itcmain/src/component/page/home.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
-function home() {
+function Home() {
   return (
     <div>
-      
       <h1>hellooo</h1>
     </div>
   )
 }
 
-export default home;
+export default Home
 
