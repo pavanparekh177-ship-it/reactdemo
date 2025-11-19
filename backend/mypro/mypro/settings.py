1a139807204faf954e6f5d6c7f3aa7e1a04f467d
Pavan Parekh <pavanparekh177@gmail.com>
2025-11-19 07:25:06 +0530

    django

--- backend/mypro/mypro/settings.py
+++ backend/mypro/mypro/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mypro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [Temp],
+        'DIRS': ['Temp'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
